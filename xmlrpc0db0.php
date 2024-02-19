cea777d570a963bc2fe0a321027292f06d924427
Jakob <mikrobi@github.com>
2024-02-19 10:17:27 +0100

    Replace hardcoded URL

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://www.neuropsychologie-isny.de</homePageLink>
+		<homePageLink>https://neuropsychologie-isny.github.io</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://www.neuropsychologie-isny.de/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://www.neuropsychologie-isny.de/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://www.neuropsychologie-isny.de/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://www.neuropsychologie-isny.de/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://www.neuropsychologie-isny.de/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://neuropsychologie-isny.github.io/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://neuropsychologie-isny.github.io/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://neuropsychologie-isny.github.io/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://neuropsychologie-isny.github.io/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://neuropsychologie-isny.github.io/wp-json/" />
 			</apis>
 	</service>
 </rsd>
